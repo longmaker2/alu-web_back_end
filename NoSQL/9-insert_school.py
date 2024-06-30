ccd94c9696d745fec5f0c9743ccae98518c771c4
longmaker2 <l.deng@alustudent.com>
2024-07-01 01:44:48 +0200

    trying new approach

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-"""python script"""
-from pymongo import MongoClient
+""" pymongo
+"""
 
 
 def insert_school(mongo_collection, **kwargs):
-    """inserts a new document in the collection"""
+    """ inserts a new document in a mongo collection
+    """
     new_document = mongo_collection.insert_one(kwargs)
-    new_document.inserted_id
+    return new_document.inserted_id
